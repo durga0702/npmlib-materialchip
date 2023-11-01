6437aadff83ec3a5f7a3660093f71e7bd1f33df3
durga0702 <durga@sparkouttech.com>
2023-11-01 09:39:36 +0530

    mat chip material work checking

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NpmlibMaterialchipModule } from 'npmlib-materialchip';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { NpmlibMaterialchipModule } from 'npmlib-materialchip';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NpmlibMaterialchipModule
+    NpmlibMaterialchipModule,
+    NoopAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
